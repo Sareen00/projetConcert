266e65e691c66b4d5ab4ba99108bf3bfcee6d4ac
Lucas Baristol <3neary3@gmail.com>
2023-03-08 01:56:40 +0100

    ajout tp form

--- src/Controller/ConcertController.php
+++ src/Controller/ConcertController.php
@@ -12,10 +12,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ConcertController extends AbstractController
 {
-
-
     #[Route('/concert', name: 'concert_list')]
-    public function showArtists(ManagerRegistry $doctrine):Response
+    public function showAllConcerts(ManagerRegistry $doctrine):Response
     {
         $concerts = $doctrine->getRepository(Concert::class)->findAll();
         return $this->render('concert/concertList.html.twig', ['concerts' => $concerts]);
@@ -24,16 +22,70 @@ class ConcertController extends AbstractController
 
 
     #[Route('/concert/{id}', name: 'concert_show', requirements: ['id' => '\d+'])]
-    public function showArtist(ManagerRegistry $doctrine,$id):Response
+    public function showConcert(ManagerRegistry $doctrine,$id):Response
     {
-        $concert = $doctrine->getRepository(ConcertType::class)->find($id);
+        $concert = $doctrine->getRepository(Concert::class)->find($id);
         return $this->render('concert/concertShow.html.twig', ['concert' => $concert]);
 
     }
 
 
+    
+    #[Route('/concert/success', name: 'concert_success')]
+    public function successConcert():Response
+    {
+        return $this->render('concert/concertSuccess.html.twig');
+    }
+
+
+
+
+
+    #[Route('/concert/delete/{id}', name: 'concert_delete', requirements: ['id' => '\d+'])]
+    public function deleteConcert(ManagerRegistry $doctrine,$id)
+    {        
+        $concert = new Concert();
+        $concert = $doctrine->getRepository(Concert::class)->find($id);
+        $entityManager = $doctrine->getManager();
+        $entityManager->remove($concert);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('concert_list');
+    }
+
+
+
+    #[Route('/concert/edit/{id}', name: 'concert_edit', requirements: ['id' => '\d+'])]
+    public function editConcert(Request $request,ManagerRegistry $doctrine,$id)
+    {        
+
+        $concert =  $doctrine->getRepository(Concert::class)->find($id);
+        $form = $this->createForm(ConcertType::class,$concert);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $concert = $form->getData();
+
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($concert);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('concert_success');
+        }
+
+        return $this->render('concert/concertNew.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+
+
+
+
+
     #[Route('/concert/create', name: 'concert_create')]
-    public function createConcert(Request $request): Response
+    public function createConcert(Request $request,ManagerRegistry $doctrine): Response
     {
         $concert = new Concert();
         $form = $this->createForm(ConcertType::class,$concert);
@@ -43,7 +95,8 @@ class ConcertController extends AbstractController
 
             $concert = $form->getData();
 
-            $entityManager = $this->getDoctrine()->getManager();
+            // $entityManager = $this->getDoctrine()->getManager();
+            $entityManager = $doctrine->getManager();
             $entityManager->persist($concert);
             $entityManager->flush();
 
